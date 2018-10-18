8179c43e62ce90463a3f03d6aefcf6359e45c780
Jcontratto <jmc22@hotmail.com>
2018-10-18 18:31:25 -0400

    first commit

---
+++ clicky-game/src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+// import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import Card from "./components/Card";
+import Wrapper from "./components/Wrapper";
+import characters from "./characters.json";
+import './App.css';
+
+class App extends Component {
+
+    state = {
+        characters
+    };
+
+    render() {
+        return (
+            <Wrapper>
+                <Header>Futurama Clicky Game</Header>
+                {this.state.characters.map(character => (
+                    <Card
+                        id={character.id}
+                        key={character.id}
+                        name={character.name}
+                        image={character.image}
+                    />
+                ))}
+            </Wrapper>
+        );
+    }
+}
+
+export default App;
+
