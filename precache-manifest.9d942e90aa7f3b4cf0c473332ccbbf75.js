675aad9cc4ec41342080518f791f725458d88bbd
Jcontratto <jmc22@hotmail.com>
2018-10-20 14:07:51 -0400

    push

---
+++ precache-manifest.9d942e90aa7f3b4cf0c473332ccbbf75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "29557695c066b63ea1eb96912058d174",
+    "url": "/Clicky-Game/static/media/background.29557695.jpg"
+  },
+  {
+    "revision": "2f9ed21177874c072eef",
+    "url": "/Clicky-Game/static/js/runtime~main.2f9ed211.js"
+  },
+  {
+    "revision": "69fb075c02352c684456",
+    "url": "/Clicky-Game/static/js/main.69fb075c.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/Clicky-Game/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "69fb075c02352c684456",
+    "url": "/Clicky-Game/static/css/main.38f6ebfb.chunk.css"
+  },
+  {
+    "revision": "379a82991fb3fb14aa4d899dc9fadc62",
+    "url": "/Clicky-Game/index.html"
+  }
+];
